b72a5456aa15bd4b9947a73f62be494ae892a399
student <soncalajsuvanool2@gmail.com>
2024-12-10 11:14:12 +0700

    commit2

--- products/actions/delete.php
+++
@@ -1,9 +0,0 @@
-<?php
-/** @var PDO $pdo */
-$pdo = require $_SERVER['DOCUMENT_ROOT'] . '/db.php';
-
-$id= $_GET['id'];
-
-$pdo->query('DELETE FROM products WHERE id =' . $id);
-
-header('Location: /products/');
--- products/actions/update.php
+++
@@ -1,8 +0,0 @@
-<?php
-/** @var PDO $pdo */
-$pdo = require $_SERVER['DOCUMENT_ROOT'] . '/db.php';
-$id= $_POST['id'];
-$name= $_POST['name'];
-$products = $pdo->query("UPDATE products SET name='$name' WHERE id=" . $id);
-header('Location: /products/');
-
